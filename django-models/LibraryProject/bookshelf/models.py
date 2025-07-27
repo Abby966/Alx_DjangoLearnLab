2dd07bcfd2a91042f03b962fbffcbca0094fe217
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 21:57:27 +0300

    Update models.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
