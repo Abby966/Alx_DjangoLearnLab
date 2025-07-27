5613cf729b1db2f3ab500da3f232c5f2a35becca
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 23:30:25 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth.decorators import permission_required
 
+@permission_required('bookshelf.can_view_books', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
