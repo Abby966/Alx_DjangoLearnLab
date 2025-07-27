a2556d13448706f66884945302b07ee228e0d1fa
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 23:25:12 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Book
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 def index(request):
     return HttpResponse("Welcome to the Book Shelf!")
 
