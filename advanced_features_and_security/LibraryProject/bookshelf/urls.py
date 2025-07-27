3b04aa0cfa7380fb9a8a35ab27e6ce16f5ca15e3
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-28 00:04:23 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import example_form_view
 
 urlpatterns = [
     path('', views.index, name='index'),  # This will serve /books/
+    path('example-form/', example_form_view, name='example-form'),
 ]
