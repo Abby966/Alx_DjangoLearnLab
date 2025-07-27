861df58656585e25b9b8990367785bc358804c60
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 23:59:36 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/relationship_app/forms.py
+++ advanced_features_and_security/LibraryProject/relationship_app/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Book
+
+class BookSearchForm(forms.Form):
+    title = forms.CharField(max_length=100)
 
 class RegisterForm(UserCreationForm):
     class Meta:
