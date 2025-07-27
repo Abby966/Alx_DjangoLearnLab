44354f997e9fe82a0d0a54d7faa69cacb6be8ca0
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-28 00:07:53 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Book
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+
+def example_form_view(request):
+    form = ExampleForm()
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Do something with the valid data (e.g., search books)
+            title = form.cleaned_data['title']
+            # For now, just pass it to the template
+            return render(request, 'form_example.html', {'form': form, 'title': title})
+    return render(request, 'form_example.html', {'form': form})
+
 # ✅ Good (safe from SQL injection)
 Book.objects.filter(title__icontains=query)
 
