6cca55c0c0f698ed055cc8d2df548bee087b3da1
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 22:02:13 +0300

    Update models.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-class Library(models.Model):
-    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
