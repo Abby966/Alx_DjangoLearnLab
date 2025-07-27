5976158ac33847859b01207b2fe30fa0919e5db8
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 22:24:41 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView  # ✅ checker wants this
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
