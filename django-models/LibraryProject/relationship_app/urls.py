f5ed227d28082ff28548c2a70c267dd12a93393a
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 22:47:29 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
-
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
+    path('member-dashboard/', views.member_view, name='member_view'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
