44331d67023d42a73dfa26daa3072f52321c4150
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 22:31:31 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
 from .models import Book  # <-- satisfies "Book.objects.all()"
 from .models import Library  # <-- satisfies "from .models import Library"
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your actual homepage view name
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 
 # Function-based view
 def list_books(request):
