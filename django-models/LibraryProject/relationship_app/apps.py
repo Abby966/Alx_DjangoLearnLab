270bdec3c160abc1cb76aec001bb0967c49a23f3
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-07-27 22:43:55 +0300

    Update apps.py

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+def ready(self):
+    import relationship_app.signals
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
