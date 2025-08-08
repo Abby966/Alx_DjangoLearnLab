e39469cf879529b5c065ff0a5c0cfa62c15a262e
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-08 15:22:40 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets, permissions
 
 class BookViewSet(viewsets.ModelViewSet):
-    """
-    A ViewSet for viewing and editing Book instances.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
