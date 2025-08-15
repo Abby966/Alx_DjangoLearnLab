ce74e17bbd65ea9d5e8abbe5f5d87b90ffc3734c
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-15 22:22:33 +0300

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -12,8 +12,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            # separate file ensures tests don't touch dev/prod DB
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        },
+    }
+}
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,3 +140,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
