2f7ecc3cef9678ca827001fd3995287628cde990
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-15 23:19:41 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
 # blog/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
+from .forms import RegisterForm, ProfileForm
 
 def index(request):
     posts = Post.objects.select_related("author").all()
     return render(request, "blog/index.html", {"posts": posts})
 
-# Create your views here.
+def register(request):
+    """User sign-up using our extended form."""
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created! You can log in now.")
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    """View/edit the logged-in user's profile."""
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated.")
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
