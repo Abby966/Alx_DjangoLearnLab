2fe0c2c8f410cf28be29abdb84ce2f4c89544d63
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-16 10:11:39 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,19 @@
 # blog/urls.py
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
-    CommentCreateView, CommentUpdateView, CommentDeleteView
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    CommentCreateView,
+    CommentDeleteView,
+    CommentUpdateView,  
+    PostsByTagListView,   # NEW
+    PostSearchListView,   # NEW
 )
+
+
 
 urlpatterns = [
     # Posts
@@ -17,5 +27,8 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),  # ✅ checker wants this
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+
+    path("tags/<slug:slug>/", PostsByTagListView.as_view(), name="posts-by-tag"),
+    path("search/", PostSearchListView.as_view(), name="search"),
 ]
 
