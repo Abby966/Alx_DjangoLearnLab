66e878fd31703827988c66101b5626682872d5fb
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-16 09:32:31 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse_lazy
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 class PostListView(ListView):
     model = Post
@@ -40,4 +43,40 @@ class PostDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy("post-list")
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"  # we’ll also post from the post detail page
 
+    def form_valid(self, form):
+        # post pk is in the URL as "pk"
+        post = get_object_or_404(Post, pk=self.kwargs["pk"])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+class CommentAuthorRequiredMixin(UserPassesTestMixin):
+    raise_exception = True
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+class CommentUpdateView(LoginRequiredMixin, CommentAuthorRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+class CommentDeleteView(LoginRequiredMixin, CommentAuthorRequiredMixin, DeleteView):
+    model = Comment
+    template_name = "blog/comment_confirm_delete.html"
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
