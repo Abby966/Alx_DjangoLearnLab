9383adc63db22436a3c1f4dd0314eb237d172465
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-28 22:59:44 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -116,3 +116,16 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
+INSTALLED_APPS += ['posts']
+REST_FRAMEWORK.update({
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+})
+
+
+
+
