774fe663573a79dce16bf34a4776f70c4cbd51f8
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-08-28 22:34:06 +0300

    Update settings.py

--- social media_api/social_media_api/settings.py
+++ social media_api/social_media_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "accounts",
     "rest_framework",
     "rest_framework.authtoken",
     "users.apps.UsersConfig",
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
